bed1c600438941d4590779e4f758d6124681904e
Shuru63 <shubhamkuargarg63@gmail.com>
2024-02-29 19:21:58 +0530

    complete all page of s2 portfolio

--- sarva_suvihan/src/Component/Services.jsx
+++ sarva_suvihan/src/Component/Services.jsx
@@ -1,9 +1,121 @@
 import React from 'react'
-
+import phone from '../static_data/web_content/mobilepic.png'
+import servicesImg1 from '../static_data/web_content/idrail.jpg'
+import servicesImg2 from '../static_data/web_content/awms.jpg'
+import servicesImg3 from '../static_data/web_content/diot.jpg'
+import servicesImg4 from '../static_data/web_content/water.jpg'
+import userInterface from '../static_data/web_content/@S2_Portfolio.jpg'
+import { Link } from 'react-router-dom';
 const Services = () => {
   return (
     <div>
-      
+      <section className=" servicesbg-1">
+        <div className='carrier-top-cover'>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="block text-center  mb-4" data-aos="fade-up">
+                  <h1 className="text-capitalize  text-white">Services </h1>
+                  <span className="text-white ">What we do</span>
+
+                  <p></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="section service border-top  pb-5 p-4">
+        <div className="container ">
+          <div className="row justify-content-center">
+            <div className="col-lg-7 text-center">
+              <div className="section-title" data-aos="fade-up">
+                <span className="h6 text-color">Our Services</span>
+                <h2 className="mt-3 content-title ">We provide the softwere and IOT Product to  their Solve daily life problem </h2>
+              </div>
+            </div>
+          </div>
+
+          <div className="row justify-content-center p-4">
+            <div className="col-lg-4 col-md-6 col-sm-6">
+              <div className="service-item mb-5">
+                <img src={servicesImg1} className="card-img-top" alt="..." />
+                <h4 className="mb-3">
+                  RailMadad Analytics.</h4>
+                <p>Our expertise lies in extracting valuable insights from RailMadad Data, enabling informed decisions and actionable outcomes. By analyzing this data, we drive a deeper understanding of rail operations, enhancing efficiency, safety, and overall performance.                  .</p>
+                <a href="#" className="service-btn btn-primary">Read More</a>
+              </div>
+
+            </div>
+
+            <div className="col-lg-4 col-md-6 col-sm-6">
+              <div className="service-item mb-5">
+                <img src={userInterface} className="card-img-top" alt="..." />
+                <h4 className="mb-3">Interface Design.</h4>
+                <p><span className='bold'>Our Software: Where Innovation Meets User-Friendly Design</span>Experience the future of technology with an intuitive interface that makes Software accessible to everyone.
+                  Our software's user-friendly design puts the power of IOT at your fingertips,
+                  simplifying complex tasks and enhancing your workflow.</p>
+                <a href="#" className="service-btn btn-primary">Read More</a>
+              </div>
+
+            </div>
+
+            <div className="col-lg-4 col-md-6 col-sm-6">
+              <div className="service-item mb-5">
+                <img src={phone} className="card-img-top" alt="..." />
+                <h4 className="mb-3">SwachhDNR.</h4>
+                <p>Our integrated platform, comprising a website and mobile app, streamlines employee performance record management. Currently utilized at Patna and Danapur Junction, it offers a user-friendly and effective solution. Simplifying the process, it ensures efficient tracking and assessment of employee performance records.</p>
+                <a href="#" className="service-btn btn-primary">Read More</a>
+              </div>
+
+            </div>
+
+            <div className="col-lg-4 col-md-6 col-sm-6">
+              <div className="service-item mb-5 mb-lg-0">
+                <img src={servicesImg4} className="card-img-top" alt="..." />
+                <h4 className="mb-3">AWMS - Automated Water Monitoring System.</h4>
+                <p>Revolutionizing train water management, our IoT solution provides real-time updates on water tank storage to upcoming stations. Through an attached GSM module, this innovative system ensures timely communication, optimizing water availability and utilization during train journeys.</p>
+                <a href="#" className="service-btn btn-primary">Read More</a>  </div>
+
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-6">
+              <div className="service-item mb-5 mb-lg-0">
+                <img src={servicesImg3} className="card-img-top" alt="..." />
+                <h4 className="mb-3">Automated Monitoring System.</h4>
+                <p>This integrated solution merges embedded systems and software to real-time monitor solar panel efficiency. It records solar cell and battery voltage, facilitating remote device oversight. Presently deployed at IIT Patna for enhanced performance tracking.</p>
+                <a href="#" className="service-btn btn-primary">Read More</a></div>
+
+            </div>
+
+            <div className="col-lg-4 col-md-6 col-sm-6">
+              <div className="service-item mb-5 mb-lg-0">
+                <img src={servicesImg2} className="card-img-top" alt="..." />
+                <h4 className="mb-3">ACP Monitoring.</h4>
+                <p>An IoT-driven railway enhancement system records and tallies Alarm Chain Pulling (ACP) instances, capturing GPS location, trigger time, and resultant train delay. Real-time transmission via attached GSM ensures seamless data relay, optimizing operations and safety measures.</p>
+                <a href="#" className="service-btn btn-primary mt-3">Read More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className=" service-contact mb-4">
+        <div className='cover-cta-2'>
+          <div className="container ">
+            <div className="cta-block p-5 rounded">
+              <div className="row justify-content-center align-items-center" data-aos="fade-up">
+                <div className="col-lg-7 text-center text-lg-left " >
+                  <span className=" text-white">For Every type software</span>
+                  <h2 className="mt-2 text-white" >Commit Your softwere to Our Skilled and Experienced Professionals</h2>
+                </div>
+                <div className="col-lg-4 text-center text-lg-right mt-4 mt-lg-0">
+                  <Link to="/contact-us" className="btn btn-main btn-round-full float-lg-right ">Contact Us</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
     </div>
   )
 }
--- sarva_suvihan/src/Component/porfolio.jsx
+++ sarva_suvihan/src/Component/porfolio.jsx
@@ -1,9 +1,204 @@
 import React from 'react'
+import Slider from 'react-slick';
+import special from '../static_data/web_content/4_Ashwini Kumar Choubey_Minister of State for Consumer Affairs, Food and Public Distribution of India.jpeg'
+import special2 from '../static_data/web_content/NEILIT_Ropar.jpeg'
+import homepage from '../static_data/web_content/homepage.jpeg'
+import servicesImg1 from '../static_data/web_content/trophy1.jpg'
+import servicesImg2 from '../static_data/web_content/trophypdl.jpg'
+import gpai1 from '../static_data/web_content/5_Special_Secretary_Bihar.jpeg'
+import gpai2 from '../static_data/web_content/Col_SurendraKumarRohilla_Director_MoDefence.jpeg'
+import gpai3 from '../static_data/web_content/NEILIT_Ropar.jpeg'
+import gpai4 from '../static_data/web_content/STPI_DG_and_Chairman_STPINEXT_Arvind_Kumar.jpeg'
+import servicesImg4 from '../static_data/web_content/certificatenitish.jpg'
+import patnaaward from '../static_data/web_content/patnaAward.jpg'
 
 const porfolio = () => {
+  var settings_services = {
+    dots: true,
+    speed: 1500,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: null,
+  }
   return (
     <div>
-      
+      <section className=" potfoliobg-1">
+        <div className=' potfoliobg-cover'>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="block text-center  mb-4" data-aos="fade-up">
+                  <h1 className="text-capitalize  text-white">Portfolio </h1>
+                  <span className="text-white ">Latest Work</span>
+
+                  <p></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="section service border-top  pb-5 p-4">
+        <div className="container ">
+          <div className="row justify-content-center">
+            <div className="col-lg-7 text-center">
+              <div className="section-title" data-aos="fade-up">
+                <span className="h6 text-color">Our works</span>
+                <h2 className="mt-3 content-title ">We have done lots of works, lets check some</h2>
+              </div>
+            </div>
+          </div>
+
+          <div className='portfolio-card-container '>
+            <div className='portfolio-card'>
+              <Slider {...settings_services}>
+
+                <div className="card card-show" style={{ width: "15rem;" }}>
+                  <div className='img-slide'><img src={homepage} className="card-img-top" alt="..." /></div>
+                  <div className="card-body text-center">
+                    <h5 className="card-title ">Global Partnership Submit - 2023</h5>
+                    <p className="card-text pb-4">
+
+                      We are thrilled to announce our active participation in the Global Partnership Summit 2023, a significant platform bringing together visionaries, innovators, and thought leaders from around the world. This summit serves as a pivotal forum for collaboration, knowledge exchange, and fostering meaningful partnerships. Our engagement reflects our commitment to global connectivity, collaboration, and addressing challenges through innovative solutions. As we contribute to the dynamic discussions and collaborative initiatives at the summit, we are eager to explore synergies, share insights, and shape the future of technology and business on a global scale. This participation underscores our dedication to creating a positive impact, fostering international collaboration, and contributing to the collective efforts aimed at shaping a sustainable and progressive global future. We look forward to meaningful interactions, forging new
+                      connections, and leveraging this platform to drive positive change and innovation on a global stage. </p>
+
+                  </div>
+                </div>
+                <div className="card card-show" style={{ width: "15rem;" }}>
+                  <div className='img-slide'>   <img src={special} className="card-img-top" alt="..." /></div>
+                  <div className="card-body text-center">
+                    <h5 className="card-title">Meeting  with Ashwini kumar Chowaye (Minister of State for Consumer Affairs)</h5>
+                    <p className="card-text pb-4">During the Global Partnership Summit 2023, it was an honor to meet with Ashwini Kumar Choubey, the Minister of State for Consumer Affairs, Food, and Public Distribution of India. The meeting provided a valuable platform to discuss collaborative initiatives aimed at enhancing consumer welfare, ensuring food security, and optimizing public distribution systems. We engaged in constructive dialogues on leveraging technology and innovative solutions to address evolving challenges in these critical sectors. The insightful exchange of ideas and commitment to fostering global partnerships reaffirmed our collective dedication to advancing the well-being of consumers and optimizing public services. This encounter with Minister Ashwini Kumar Choubey marked a significant step toward collaborative efforts for sustainable development and inclusive governance. We look forward to contributing actively to the shared vision of fostering a resilient and consumer-centric future. </p>
+
+                  </div>
+                </div>
+                <div className="card card-show" style={{ width: "15rem;" }}>
+                  <div className='img-slide'>   <img src={special2} className="card-img-top" alt="..." /></div>
+                  <div className="card-body text-center">
+                    <h5 className="card-title">convergence of diverse industrialists and innovators</h5>
+                    <p className="card-text pb-4">The Global Partnership Summit 2023 was a convergence of diverse industrialists and innovators, fostering collaboration and knowledge exchange. At this transformative event, we had the privilege of engaging with a spectrum of industry leaders, visionaries, and influencers from across the globe. It provided a unique platform to explore synergies, share insights, and forge partnerships that transcend geographical boundaries. Discussions spanned a myriad of industries, from technology and finance to sustainability and healthcare, reflecting the interconnected nature of our global ecosystem. Through these meaningful interactions, we not only expanded our network but also gained valuable perspectives that will undoubtedly shape the future of our endeavors. The Global Partnership Summit served as a catalyst for cross-sector collaboration, amplifying the impact of our collective efforts on a global scale. As we reflect on this enriching experience, we look forward to translating these connections into tangible partnerships that drive innovation, sustainability, and progress across industries worldwide</p>
+
+                  </div>
+                </div>
+                <div className="card card-show" style={{ width: "15rem;" }}>
+                  <div className='img-slide'><img src={servicesImg1} className="card-img-top" alt="..." /></div>
+                  <div className="card-body text-center">
+                    <h5 className="card-title ">Smart Bihar Hackthon-2023</h5>
+                    <p className="card-text pb-4">
+                      In the Smart Bihar Hackathon 2023, our team\
+                      emerged victorious, securing the first prize\
+                      for our innovative startup idea focused on IT services
+                      and IoT software solutions. Our winning concept
+                      reflects a commitment to harnessing cutting-edge
+                      technology to address real-world challenges in Bihar.
+                      By blending expertise in information technology and
+                      IoT, our startup envisions transformative solutions
+                      that enhance connectivity, efficiency, and sustainability.
+                      This achievement underscores our dedication to
+                      driving positive change through innovation. As we forge ahead, we are poised to bring our winning idea to fruition, leveraging our skills and passion
+                      to contribute meaningfully to the technological landscape. Our success
+                      in the hackathon serves as a testament to the creativity, teamwork,
+                      and forward-thinking approach that define our
+                      venture. With this accolade, we are eager to showcase our capabilities and explore opportunities for
+                      collaboration and growth. The journey doesn't end here; it marks the beginning of a promising venture that aims to make a lasting impact in the realm of IT services and IoT software development. Join us on this exciting journey as we continue to push boundaries and pioneer
+                      advancements in the tech industry. </p>
+
+                  </div>
+                </div>
+                <div className="card card-show" style={{ width: "15rem;" }}>
+                  <div className='img-slide'>  <img src={servicesImg2} className="card-img-top" alt="..." /> </div>
+                  <div className="card-body text-center">
+                    <h5 className="card-title">Bihar Industries Association Award</h5>
+                    <p className="card-text pb-4">We are immensely proud to announce that our startup has clinched the prestigious Bihar Industries
+                      Association Award, securing the top position in the IT Services and IoT Software category. This
+                      recognition reaffirms our commitment to innovation, excellence, and impactful solutions.
+                      At the intersection of cutting-edge Information Technology services and pioneering IoT software,
+                      we have consistently pushed boundaries to deliver unparalleled value to our clients. This accolade
+                      not only reflects our team's dedication but also underscores the transformative impact of our services
+                      on the industry. As we celebrate this milestone, we look forward to continuing our journey of technological
+                      innovation, driving positive change, and delivering exceptional results for our clients. This award serves as
+                      a testament to our vision, hard work, and the collective effort of our talented team, motivating us to reach
+                      new heights in the dynamic landscape of IT and IoT.
+                    </p>
+
+                  </div>
+                </div>
+
+                <div className="card card-show" style={{ width: "15rem;" }}>
+                  <div className='img-slide'>    <img src={servicesImg4} className="card-img-top" alt="..." /></div>
+                  <div className="card-body text-center">
+                    <h5 className="card-title">Destination Bihar Expo-2024</h5>
+                    <p className="card-text pb-4">Participation in the Destination Bihar Expo-2024 marks a significant milestone for us. Engaging in this prestigious event allows us to showcase our commitment to excellence, innovation, and contributing to the growth of Bihar's economic landscape. By actively participating, we aim to forge valuable connections, explore collaborations, and highlight the unique offerings of our business.
+
+                      Receiving a certificate from the Destination Bihar Expo-2024 is an esteemed acknowledgment of our presence and contribution. This recognition underscores our dedication to excellence and showcases our alignment with the goals and vision of the expo. The certificate serves as a testament to our commitment to quality, innovation, and the positive impact we aspire to make in the vibrant economic and cultural milieu of Bihar. We are honored to be part of this event, and the certificate symbolizes our dedication to advancing and contributing to the flourishing business ecosystem in Bihar.</p>
+
+                  </div>
+                </div>
+              </Slider>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="section service border-top  pb-5 p-4">
+        <div className="container ">
+          <div className="row justify-content-center">
+            <div className="col-lg-7 text-center">
+              <div className="section-title" data-aos="fade-up">
+                <h2 className="mt-3 content-title ">Galary</h2>
+              </div>
+            </div>
+          </div>
+
+         <div className='galary'>
+       
+<div class="row">
+  <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+    <img
+      src={gpai1}
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt="Boat on Calm Water"
+    />
+
+    <img
+      src={gpai2}
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt="Wintry Mountain Landscape"
+    />
+  </div>
+
+  <div class="col-lg-4 mb-4 mb-lg-0">
+    <img
+      src={gpai3}
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt="Mountains in the Clouds"
+    />
+
+    <img
+      src={gpai4}
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt="Boat on Calm Water"
+    />
+  </div>
+
+  <div class="col-lg-4 mb-4 mb-lg-0">
+    <img
+      src={homepage}
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt="Waves at Sea"
+    />
+
+    <img
+      src={patnaaward}
+      class="w-100 shadow-1-strong rounded mb-4"
+      alt="Yosemite National Park"
+    />
+  </div>
+</div>
+
+         </div>
+        </div>
+      </section>
     </div>
   )
 }
