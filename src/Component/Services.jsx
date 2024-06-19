9b444d94653ce882983566f0499cb9df945e2e4b
unstoppableayush <ayushkum884@gmail.com>
2024-06-19 13:43:14 +0530

    layout fixes of home page and services page

--- src/Component/Services.jsx
+++ src/Component/Services.jsx
@@ -48,7 +48,7 @@ const Services = () => {
             <div className="col-lg-7 text-center">
               <div className="section-title" data-aos="fade-up">
                 <span className="h6 text-color">Our Services</span>
-                <h2 className="mt-3 content-title ">We provide the softwere and IOT Product to  their Solve daily life problem </h2>
+                <h2 className="my-3 content-title ">We provide the softwere and IOT Product to  their Solve daily life problem </h2>
               </div>
             </div>
           </div>
@@ -57,10 +57,10 @@ const Services = () => {
             <div className="col-lg-4 col-md-6 col-sm-6">
               <div className="service-item mb-5">
               <LazyLoad placeholder={<Skeleton width={240} height={240} />}><img src={servicesImg1} className="card-img-top" alt="..."  loading="lazy" /></LazyLoad>  
-                <h4 className="mb-3">
+                <h4 className="my-3">
                   RailMadad Analytics.</h4>
-                <p>Our expertise lies in extracting valuable insights from RailMadad Data, enabling informed decisions and actionable outcomes. By analyzing this data, we drive a deeper understanding of rail operations, enhancing efficiency, safety, and overall performance.                  .</p>
-                <a href="#" className="service-btn btn-primary" onClick={() => setShowModalFront(true)}>Read More</a>
+                <p className='mb-3'>Our expertise lies in extracting valuable insights from RailMadad Data, enabling informed decisions and actionable outcomes. By analyzing this data, we drive a deeper understanding of rail operations, enhancing efficiency, safety, and overall performance.                  .</p>
+                <a href="#" className="service-btn btn-primary " onClick={() => setShowModalFront(true)}>Read More</a>
               </div>
 
             </div>
@@ -68,8 +68,8 @@ const Services = () => {
             <div className="col-lg-4 col-md-6 col-sm-6">
               <div className="service-item mb-5">
               <LazyLoad placeholder={<Skeleton width={240} height={240} />}><img src={userInterface} className="card-img-top" alt="..."  loading="lazy" /></LazyLoad>
-                <h4 className="mb-3">Interface Design.</h4>
-                <p><span className='bold'>Our Software: Where Innovation Meets User-Friendly Design</span>Experience the future of technology with an intuitive interface that makes Software accessible to everyone.
+                <h4 className="my-3">Interface Design.</h4>
+                <p className='mb-3'><span className='bold'>Our Software: Where Innovation Meets User-Friendly Design</span>Experience the future of technology with an intuitive interface that makes Software accessible to everyone.
                   Our software's user-friendly design puts the power of IOT at your fingertips,
                   simplifying complex tasks and enhancing your workflow.</p>
                 <a href="#" className="service-btn btn-primary"onClick={() => setShowModalUI(true)}>Read More</a>
@@ -80,8 +80,8 @@ const Services = () => {
             <div className="col-lg-4 col-md-6 col-sm-6">
               <div className="service-item mb-5">
               <LazyLoad placeholder={<Skeleton width={240} height={240} />}><img src={phone} className="card-img-top" alt="..."  loading="lazy" /></LazyLoad>  
-                <h4 className="mb-3">SwachhDNR.</h4>
-                <p>Our integrated platform, comprising a website and mobile app, streamlines employee performance record management. Currently utilized at Patna and Danapur Junction, it offers a user-friendly and effective solution. Simplifying the process, it ensures efficient tracking and assessment of employee performance records.</p>
+                <h4 className="my-3">SwachhDNR.</h4>
+                <p className='mb-3'>Our integrated platform, comprising a website and mobile app, streamlines employee performance record management. Currently utilized at Patna and Danapur Junction, it offers a user-friendly and effective solution. Simplifying the process, it ensures efficient tracking and assessment of employee performance records.</p>
                 <a href="#" className="service-btn btn-primary" onClick={() => setShowModal(true)}>Read More</a>
               </div>
 
@@ -90,16 +90,16 @@ const Services = () => {
             <div className="col-lg-4 col-md-6 col-sm-6">
               <div className="service-item mb-5 mb-lg-0">
               <LazyLoad placeholder={<Skeleton width={240} height={240} />}><img src={servicesImg4} className="card-img-top" alt="..."  loading="lazy" /></LazyLoad>  
-                <h4 className="mb-3">AWMS - Automated Water Monitoring System.</h4>
-                <p>Revolutionizing train water management, our IoT solution provides real-time updates on water tank storage to upcoming stations. Through an attached GSM module, this innovative system ensures timely communication, optimizing water availability and utilization during train journeys.</p>
+                <h4 className="my-3">AWMS - Automated Water Monitoring System.</h4>
+                <p className='mb-3'>Revolutionizing train water management, our IoT solution provides real-time updates on water tank storage to upcoming stations. Through an attached GSM module, this innovative system ensures timely communication, optimizing water availability and utilization during train journeys.</p>
                 <a href="#" className="service-btn btn-primary" onClick={() => setShowModalAwms(true)}>Read More</a>  </div>
 
             </div>
             <div className="col-lg-4 col-md-6 col-sm-6">
               <div className="service-item mb-5 mb-lg-0">
               <LazyLoad placeholder={<Skeleton width={240} height={240} />}><img src={servicesImg3} className="card-img-top" alt="..."  loading="lazy" /></LazyLoad>   
-                <h4 className="mb-3">Automated Monitoring System.</h4>
-                <p>This integrated solution merges embedded systems and software to real-time monitor solar panel efficiency. It records solar cell and battery voltage, facilitating remote device oversight. Presently deployed at IIT Patna for enhanced performance tracking.</p>
+                <h4 className="my-3">Automated Monitoring System.</h4>
+                <p className='mb-3'>This integrated solution merges embedded systems and software to real-time monitor solar panel efficiency. It records solar cell and battery voltage, facilitating remote device oversight. Presently deployed at IIT Patna for enhanced performance tracking.</p>
                 <a href="#" className="service-btn btn-primary" onClick={() => setShowModalPyhton(true)}>Read More</a></div>
 
             </div>
@@ -107,8 +107,8 @@ const Services = () => {
             <div className="col-lg-4 col-md-6 col-sm-6">
               <div className="service-item mb-5 mb-lg-0">
               <LazyLoad placeholder={<Skeleton width={240} height={240} />}><img src={servicesImg2} className="card-img-top" alt="..."  loading="lazy" /></LazyLoad>   
-                <h4 className="mb-3">ACP Monitoring.</h4>
-                <p>An IoT-driven railway enhancement system records and tallies Alarm Chain Pulling (ACP) instances, capturing GPS location, trigger time, and resultant train delay. Real-time transmission via attached GSM ensures seamless data relay, optimizing operations and safety measures.</p>
+                <h4 className="my-3">ACP Monitoring.</h4>
+                <p className='mb-3'>An IoT-driven railway enhancement system records and tallies Alarm Chain Pulling (ACP) instances, capturing GPS location, trigger time, and resultant train delay. Real-time transmission via attached GSM ensures seamless data relay, optimizing operations and safety measures.</p>
                 <a href="#" className="service-btn btn-primary mt-3" onClick={() => setShowModalIOt(true)}>Read More</a>
               </div>
             </div>
@@ -122,16 +122,17 @@ const Services = () => {
               <div className="row justify-content-center align-items-center" data-aos="fade-up">
                 <div className="col-lg-7 text-center text-lg-left " >
                   <span className=" text-white">For Every type software</span>
-                  <h2 className="mt-2 text-white" >Commit Your softwere to Our Skilled and Experienced Professionals</h2>
+                  <h2 className="mt-2 text-white text-2xl md:text-4xl" >Commit Your softwere to Our Skilled and Experienced Professionals</h2>
                 </div>
                 <div className="col-lg-4 text-center text-lg-right mt-4 mt-lg-0">
-                  <Link to="/contact-us" className="btn btn-main btn-round-full float-lg-right ">Contact Us</Link>
+                  <Link to="/contact-us" className="btn btn-main btn-round-full float-lg-right hover:bg-blue-400">Contact Us</Link>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className='carrier-apply'>
+      </section>
+      <div className='carrier-apply'>
                 <div className=''>
                     <CModal visible={showModal} backdrop="static" aria-labelledby="ScoreNow" className="custom-modal"
                         size="xl"
@@ -333,7 +334,6 @@ const Services = () => {
                     </CModal>
                 </div>
             </div>
-      </section>
 
     </div>
   )
